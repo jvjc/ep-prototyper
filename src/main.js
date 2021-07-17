dae14ed2922cabcf950ee06cf727eb2ca1096bbe
Juan Víctor Jiménez Corzo <corzo@outlook.com>
2021-07-17 00:51:04 -0500

    Added rules to eslint

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import installElementPlus from './plugins/element'
+import { createApp } from 'vue';
+import App from './App.vue';
+import installElementPlus from './plugins/element';
 
-const app = createApp(App)
-installElementPlus(app)
-app.mount('#app')
+const app = createApp(App);
+installElementPlus(app);
+app.mount('#app');
